e0c8ac0f5ab3ad4ddce83d900b6367e5629eca21
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-15 23:33:14 +0300

    add setup

--- cli/setup.py
+++ cli/setup.py
@@ -1,1 +1,12 @@
 from setuptools import setup 
+
+setup(
+    name = 'clisample',
+    version = '1.0',
+    install_requires = ['Click',],
+    py_modules = ['clisimple',],
+    entry_points = '''
+    [consolescripts]
+    hello=clisimple:hello
+    '''
+)
