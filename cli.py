bb9654b56ac98daa8ad3311817c9102ec82d3202
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-15 13:27:38 +0300

    added click

--- cli.py
+++ cli.py
@@ -2,8 +2,11 @@ import click
 
 
 @click.command()
-def main():
-    print("Hello World")
+#basic option
+@click.option('--name', '-n')
+
+def main(name):
+    print("Hello World My name is {}".format(name))
 
 
 if __name__ == "__main__":
