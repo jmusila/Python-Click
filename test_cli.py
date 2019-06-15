4e26fa5ab976a57191ccce9869cd91a703b2236a
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-15 23:18:27 +0300

    added setup

---
+++ test_cli.py
@@ -0,0 +1,14 @@
+from io import StringIO
+from unittest import TestCase
+
+
+class Testmain(TestCase):
+
+    def test_hello(self):
+        """
+            Test print expected outcome
+        """
+        pass
+
+if __name__ == "__main__":
+    pass
