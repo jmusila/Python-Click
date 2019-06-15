4ffbac977237690bbaedeceb813bf10d5f38bbc3
jonathanmusila <jonathanmusila6@gmail.com>
2019-06-15 13:39:58 +0300

    two args

--- cli.py
+++ cli.py
@@ -2,11 +2,12 @@ import click
 
 
 @click.command()
-#basic option
-@click.option('--name', '-n')
-
-def main(name):
-    print("Hello World My name is {}".format(name))
+# basic option
+@click.option('--name', '-n', default='Jonathan', help='Firstname Description')
+# multiplr values
+@click.option('--salary', '-s', nargs=2, help='Your Monthly Salary')
+def main(name, salary):
+    print("Hello World My name is {}, my salary is {}".format(name, salary))
 
 
 if __name__ == "__main__":
